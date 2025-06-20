da250af62e7d5083ee31ff19be2bddd3cdfe9aed
Benjamin Hotop <benjamin.hotop@gmail.com>
2025-06-20 12:46:41 +0200

    add linting with ci (#4)
    * add linting with ci
    
    * change check name
    
    * change check name once again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,25 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import antfu from "@antfu/eslint-config"
 
-export default withNuxt(
-  // Your custom configs here
-)
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs"
+
+export default withNuxt(antfu({
+	stylistic: {
+		semi: false,
+		indent: "tab",
+		quotes: "double",
+	},
+	vue: true,
+	typescript: true,
+	formatters: true,
+}, {
+	rules: {
+		"perfectionist/sort-imports": ["error", {
+			tsconfigRootDir: ".",
+		}],
+		"unicorn/filename-case": ["error", {
+			case: "kebabCase",
+			ignore: ["README.md", "LICENSE.md"],
+		}],
+	},
+}))
