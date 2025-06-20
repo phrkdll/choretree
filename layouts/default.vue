916d66a1d4473bd502564f95ac154986cf9bdba6
Benjamin Hotop <benjamin.hotop@gmail.com>
2025-06-20 14:05:11 +0200

    add layout, nav and landingpage (#6)
    * add layout, nav and landingpage
    
    * fix formatting

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+	<main class="container mx-auto">
+		<AppNavbar />
+
+		<div class="mt-4">
+			<slot />
+		</div>
+	</main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="flex justify-center flex-wrap">
+		<Icon name="mynaui:tree" size="128" class="text-green-700" />
+		<h1 class="text-2xl font-semibold text-center w-full">
+			choretree
+		</h1>
+		<p class="mt-4">
+			Task management for your friends and family.
+		</p>
+	</div>
+</template>
