5578d5344a6b44003e99499dda65e25e3a21e94f
phreakadelle <benjamin.hotop@gmail.com>
2025-06-20 14:59:29 +0200

    add color mode module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
 	compatibilityDate: "2025-05-15",
 	devtools: { enabled: true },
-	modules: ["@nuxt/eslint", "@nuxt/icon"],
+	modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode"],
 	eslint: {
 		config: {
 			standalone: false,
@@ -16,4 +16,7 @@ export default defineNuxtConfig({
 		],
 	},
 	css: ["~/assets/css/main.css"],
+	colorMode: {
+		dataValue: "theme",
+	},
 })
